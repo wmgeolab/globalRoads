0f43b063f394a215a443fc59fffa127a4c91a11e
DanRunfola <dan@danrunfola.com>
2024-02-09 17:14:09 -0500

    continents

--- sourceData/retrieveData.py
+++ sourceData/retrieveData.py
@@ -7,11 +7,20 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
 import traceback
 import gc
+import requests
 
 # Constants
-PARENT_CONTINENTS = ['africa', 'antartica', 'asia', 'central-america', 'europe', 'north-america', 'south-america', 'australia-oceania']
-EXCEPTIONS = ['south-africa-and-lesotho', 'alps', 'britain-and-ireland', 'dach', 'us', 'guyana', 'guernsey-jersey', 'american-oceania', 
-              'us-midwest', 'us-northeast', 'us-pacific', 'us-south', 'us-west', 'guyana', 'united-kingdom', 'us/california', 'us/us-virgin-islands']
+
+LINKS=["https://download.geofabrik.de/antarctica-latest.osm.pbf",
+       "https://download.geofabrik.de/africa-latest.osm.pbf",
+       "https://download.geofabrik.de/asia-latest.osm.pbf",
+       "https://download.geofabrik.de/australia-oceania-latest.osm.pbf",
+       "https://download.geofabrik.de/central-america-latest.osm.pbf",
+       "https://download.geofabrik.de/europe-latest.osm.pbf",
+       "https://download.geofabrik.de/north-america-latest.osm.pbf",
+       "https://download.geofabrik.de/south-america-latest.osm.pbf"
+      ]
+
 TMPBASEPATH = "/kube/home/tmp/globalRoads"
 OUTPUTPATH = "/kube/home/git/globalRoads/sourceData/parquet"
 LOGBASEPATH = "/kube/home/logs/globalRoads"
@@ -36,35 +45,37 @@ def check_and_recreate_folder(folder_path):
     # Create the folder
     os.makedirs(folder_path)
 
-def filter_pbf_to_parquet(pbf_id):
+def filter_pbf_to_parquet(jobID):
     """
     Converts a PBF file to a Parquet file using GeoPandas, with a filtering step
     for specific road types.
-
-    :param pbf_id: ID of the PBF file.
-    :param parquet_file: Path where the Parquet file will be saved.
     """
-    pLogger(pbf_id, "INFO", "Beginning filtering and conversion to Parquet file.")
-    TMPPATH = TMPBASEPATH + "/" + str(pbf_id) + "/" + pbf_id + "-latest.osm.pbf"
-    pLogger(pbf_id, "INFO", "TMPPATH: " + str(TMPPATH))
-    parquet_file = OUTPUTPATH + "/" + str(pbf_id) + ".parquet"
-    pLogger(pbf_id, "INFO", "Parquet Path: " + str(parquet_file))
+
+    pLogger(jobID, "INFO", "Beginning filtering and conversion to Parquet file.")
+    TMPPATH = TMPBASEPATH + "/" + str(jobID) + ".osm.pbf"
+
+    pLogger(jobID, "INFO", "TMPPATH: " + str(TMPPATH))
+    parquet_file = OUTPUTPATH + "/" + str(jobID) + ".parquet"
+    pLogger(jobID, "INFO", "Parquet Path: " + str(parquet_file))
+
     # Define the subset of road types to include
     roads_subset = ['motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'residential', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link', 'living_street','track']
+    
     # Initialize the OSM object and read the PBF file
     osm = OSM(TMPPATH)
+
     # Get the data for a specific layer, here 'driving'
-    pLogger(pbf_id, "INFO", "Fetching driving network data from PBF.")
+    pLogger(jobID, "INFO", "Fetching driving network data from PBF.")
     gdf = osm.get_data_by_custom_criteria(custom_filter={'highway': True})
     # Filter the GeoDataFrame based on the roads subset
-    pLogger(pbf_id,"INFO","Filtering by road type.")
+    pLogger(jobID,"INFO","Filtering by road type.")
     roadways = gdf.loc[gdf['highway'].isin(roads_subset)]
     # Convert to GeoDataFrame if it's not already
     if not isinstance(roadways, gpd.GeoDataFrame):
-        pLogger(pbf_id, "INFO", "Convert to GDF.")
+        pLogger(jobID, "INFO", "Convert to GDF.")
         roadways = gpd.GeoDataFrame(roadways)
     # Write the filtered data to Parquet
-    pLogger(pbf_id, "INFO", "Writing to Parquet.")
+    pLogger(jobID, "INFO", "Writing to Parquet.")
     roadways['id'] = roadways['id'].astype(float).astype(int)
     roadways.to_parquet(parquet_file)
     del roadways, gdf, osm
@@ -77,99 +88,72 @@ def fetch_data(url):
     response = requests.get(url)
     return response.json()
 
-def download_feature(feature):
+def download_feature(url, jobID):
     """
-    Processes a single feature from the JSON data.
+    Downloads a layer from OSM.
     """
-    properties = feature['properties']
-    urls = properties['urls']
-    identifier = properties['id']
-    filename = identifier
-    TMPPATH = TMPBASEPATH + "/" + str(identifier)
+    TMPPATH = TMPBASEPATH + "/" + str(jobID) + ".osm.pbf"
     check_and_recreate_folder(TMPPATH)
     
-    
-    if identifier in ['antarctica', 'norcal', 'socal'] or ('parent' in properties and properties['parent'] in PARENT_CONTINENTS and identifier not in EXCEPTIONS):
-        filename = identifier
-        pLogger(filename, "INFO", "Beginning download for " + str(filename))
-        if identifier == 'antarctica':#or identifier == 'australia-oceania':
-            osm = OSM(get_data(identifier, directory = TMPPATH))
-        elif identifier == 'norcal': 
-            osm = OSM(get_data('northern_california', directory = TMPPATH))
-            filename = "northern_california"
-        elif identifier == 'socal':
-            osm = OSM(get_data('southern_california', directory = TMPPATH))
-            filename = "southern_california"
-        elif 'parent' in properties:
-            pLogger(filename, "INFO", "Parent exception identified, continuing to process " + str(filename))
-            if properties['parent'] in PARENT_CONTINENTS and identifier not in EXCEPTIONS:
-                parent = properties['parent']
-                pLogger(filename, "INFO", 'PARENT 1: ' + str(parent))
-                pLogger(filename, "INFO", 'ID: ' + str(identifier))
-                pbf_link = urls['pbf']
-                if properties['parent'] == 'north-america' and 'iso3166-2' in properties and identifier != 'us/california':
-                    updated_id = identifier.replace('us/','')
-                    filename = updated_id
-                    osm = OSM(get_data(updated_id, directory = TMPPATH))
-                elif '/' in identifier:
-                    updated_id = identifier.replace('/', '-')
-                    filename = updated_id
-                    osm = OSM(get_data(updated_id, directory = TMPPATH)) 
-                else:
-                    fp = get_data(identifier, directory = TMPPATH)
-                    osm = OSM(fp)
-            else:
-                fp = get_data(identifier, directory = TMPPATH)
-                osm = OSM(fp)
-    
-        return(filename)
-    else:
-        return("PASS")
+    try:
+        pLogger(jobID, "INFO", "Downloading: " + str(url))
+        response = requests.get(url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Open a local file in binary write mode
+            with open(TMPPATH, 'wb') as file:
+                # Write the content of the response to the file
+                file.write(response.content)
+            pLogger(jobID, "INFO", "File downloaded.")
+            return("PASS")
+        else:
+            pLogger(jobID, "CRIT", "Failed to retrieve the file. Status code: " + str(response.status_code))
+            return("FAIL")
+    except Exception as e:
+        pLogger(jobID, "CRIT", "Failed to retrieve the file with an exception. Error: " + str(e))
+        return("FAIL")
 
-def process_feature(feature):
+def process_file(url, jobID):
     # Combined function to download and convert data
     try:
-        properties = feature['properties']
-        pbf_id = properties['id']
-        parquet_file = OUTPUTPATH + "/" + str(pbf_id) + ".parquet"
-        if os.path.exists(parquet_file):
-            featureID = download_feature(feature)
-            if(featureID != "PASS"):
-                filter_pbf_to_parquet(featureID)
+        parquet_file = OUTPUTPATH + "/" + str(jobID) + ".parquet"
+        if not os.path.exists(parquet_file):
+            downloadOutcome = download_feature(url, jobID)
+            
+            if(downloadOutcome != "FAIL"):
+                filter_pbf_to_parquet(jobID)
                 pLogger("MASTER", "INFO", str(featureID) + " DONE.")
-                return([featureID,"DONE"])
+                return([jobID,"DONE"])
             else:
-                pLogger("MASTER", "INFO", str(featureID) + " PASS.")
-                return([featureID,"PASS"])
+                pLogger("MASTER_ERROR", "CRIT", "Download failed for: " + str(jobID) + " with URL: " + str(url))
+                return([jobID,"FAIL"])
         else:
-            pLogger("MASTER", "INFO", str(pbf_id) + " already exists. Skipping.")
-            return([pbf_id, "ALREADY EXISTS"])
+            pLogger("MASTER", "INFO", str(jobID) + " parquet file already exists. Skipping.")
+            return([jobID, "ALREADY EXISTS"])
     except Exception as e:
-        pLogger("MASTER_ERROR", "CRIT", "The feature was unable to be processed: " + str(feature["properties"]))
+        pLogger("MASTER_ERROR", "CRIT", "The feature was unable to be processed: " + str(jobID))
         pLogger("MASTER_ERROR", "CRIT", "E: " + str(e))
         pLogger("MASTER_ERROR", "CRIT", "Trace: " + str(traceback.format_exc()))
-        return([featureID,"ERROR"])
+        return([jobID,"ERROR"])
 
 def main():
     """
     Main function to orchestrate the fetching and processing of data.
     """
-    data_url = "https://download.geofabrik.de/index-v1.json"
-    data = fetch_data(data_url)
-    features = data['features']
-    pLogger("MASTER", "INFO", "Features: " + str(features))
-
-    for i in features:
-        output = process_feature(i)
+    for url in LINKS:
+        jobID = i.split("/")[3].split(".")[0].split("-")[0]
+        pLogger("MASTER", "INFO", "Processing: " + str(i))
+        output = process_file(url, jobID)
         pLogger("MASTER", "INFO", "Result Completed: " + str(output))
+        del output
+        gc.collect()
 
     # with ProcessPoolExecutor(max_workers=PROCESSES) as executor:
     #     futures = [executor.submit(process_feature, feature) for feature in features]
     #     for future in as_completed(futures):
     #         result = future.result()
     #         pLogger("MASTER", "INFO", "Result Completed: " + str(result))
-    
-    print(result)
+
 
 if __name__ == "__main__":
     main()
