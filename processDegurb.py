570660a8871e597575ccb4ce8cd3de70331951d5
DanRunfola <dan@danrunfola.com>
2024-03-11 15:01:01 -0400

    serial

--- processDegurb.py
+++ processDegurb.py
@@ -2,6 +2,7 @@ import geopandas
 import pymysql
 import os
 from datetime import datetime
+import sys
 
 
 mysql_config_db = {
@@ -14,6 +15,9 @@ mysql_config_db = {
 
 logging_path = "/kube/home/logs/globalRoads"
 #table: roadresults
+
+RETRIES = 5
+RESPONSEWAIT = 5
 
 def kLog(type, message, logPath=logging_path):
     podName = os.getenv('POD_NAME')
@@ -58,17 +62,48 @@ def insert_results(conn, results):
         print(f"Error: {e}")
         conn.rollback()  # Rollback in case of error
 
+def osm_request(url, retries, base_wait=1):
+    for retry in range(retries):
+        try:
+            r = requests.get(url, timeout=10, allow_redirects=False)
+            r.raise_for_status()  # Raise an exception for HTTP errors
+            res = r.json()
+            return res  # Success, return the result
+        except requests.exceptions.RequestException as e:
+            print(f"Attempt {retry + 1}/{retries}: Request failed - {str(e)}")
+            
+            if retry < retries - 1:
+                # Calculate the wait time exponentially increasing
+                wait_time = base_wait * (2 ** retry)
+                print(f"Retrying in {wait_time} seconds...")
+                time.sleep(wait_time)
+    
+    return None  # All retries failed
+
 def processPoints(pts):
     with open("./sourceData/urbanCentroids.geojson", "r") as u:
         urbanPoints = geopandas.read_file(u)
     
     urbanPoints = urbanPoints.to_crs(epsg=4326)
-
+    mindist = 9999999.0
     for index, row in pts.iterrows():
         print(index)
         print(row.geometry.x)
         print(row.geometry.y)
         print("-----")
+
+        from_lat = row.geometry.y
+        from_lon = row.geometry.x
+
+        for index_urbcent, row_urbcent in urbanPoints.iterrows():
+            to_lat = row_urbcent.geometry.y
+            to_lon = row_urbcent.geometry.x
+
+            distStr = "http://osrm:80/route/v1/driving/" + str(from_lat) + "," + str(from_lon) + ";" + str(to_lat) + "," + str(to_lon)
+
+            result = osm_request(url, RETRIES, RESPONSEWAIT)
+            print(result)
+            sys.exit()
 
     results = {}
     results["latitude"] = 10.0
